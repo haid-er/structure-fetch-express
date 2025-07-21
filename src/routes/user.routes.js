d918e98e94a6c3d50f4b72e931a72622c342681f
haid-er <malikhaiderali33@gmail.com>
2025-07-22 01:24:11 +0500

    Remove unused authentication middleware and clean up user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,8 @@
 const { fetchOrgChartJsonController, saveOrgChartDataController } = require("../controllers/user.controller");
-const {
-} = require("../validators/auth.schema");
+
 const { upload } = require("../middlewares/multer.middleware");
 const router = require("express").Router();
 
-// router.post(
-//   "/update-avatar",
-//   upload.single("avatar"),
-//   verifyJWT,
-//   updateUserAvatar
-// );
 
 router.post("/fetch-json", upload.single("orgchart"), fetchOrgChartJsonController);
 
